d4de4ac0dd7704fff047de4bead0239c103d7bec
AleLuppi <alelup@yahoo.it>
2024-10-28 23:00:40 +0100

    Fix jest imports from src
    Allow imports from "src" in test files to be named with relative path from root folder.
    Update rollup config to allow named exports.

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ function add(a: number, b: number): number {
   return a + b;
 }
 
-export default add;
+export { add };
--- test/add.test.ts
+++ test/add.test.ts
@@ -1,4 +1,4 @@
-import { add } from "../src";
+import { add } from "src";
 
 describe("Sample 'add' function test", () => {
   it("should add two numbers", () => {
