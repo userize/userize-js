7d7b8f71d4c9390e4998f25f0740aed75293fd67
AleLuppi <alelup@yahoo.it>
2024-10-28 18:04:08 +0100

    Init module with typescript and jest
    Initialize package with typescript.
    Install jest and create dummy function and relative test.

---
+++ test/add.test.ts
@@ -0,0 +1,8 @@
+import { add } from "../src";
+
+describe("Sample 'add' function test", () => {
+  it("should add two numbers", () => {
+    const result = add(2, 3);
+    expect(result).toBe(5);
+  });
+});
